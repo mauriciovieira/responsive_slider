cc1c41a781da66b0bf8cfc51fca9a0e224cd8440
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-09-21 22:04:32 -0300

    Add logic on button clicking

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,2 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const slides = document.querySelectorAll('.slide');
+  const next = document.querySelector('#next');
+  const prev = document.querySelector('#prev');
+
+  /* Auto-slide */
+  const auto = false;
+  const intervalTime = 5000;
+  let slideInterval;
+
+  const nextSlide = () => {
+    const current = document.querySelector('.current');
+
+    current.classList.remove('current');
+
+    if (current.nextElementSibling) {
+      current.nextElementSibling.classList.add('current');
+    } else {
+      slides[0].classList.add('current');
+    }
+
+    // setTimeout(() => current.classList.remove('current'));
+  }
+
+  const prevSlide = () => {
+    const current = document.querySelector('.current');
+
+    current.classList.remove('current');
+
+    if (current.previousElementSibling) {
+      current.previousElementSibling.classList.add('current');
+    } else {
+      slides[slides.length - 1].classList.add('current');
+    }
+
+    // setTimeout(() => current.classList.remove('current'));
+  }
+
+  // Button events
+  next.addEventListener('click', e => {
+    nextSlide()
+  })
+
+  // Button events
+  prev.addEventListener('click', e => {
+    prevSlide()
+  })
+
 })
